9581e0a9d8cabc0f59e07af7100e2d4a77cfa6f4
jaredgoldman <jaredgoldmandev@gmail.com>
2023-08-07 17:31:44 -0400

    FEAT: Add cloudinary support

--- config/plugins.js
+++ config/plugins.js
@@ -9,4 +9,19 @@ module.exports = ({ env }) => ({
       tracing: false,
     },
   },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 });
