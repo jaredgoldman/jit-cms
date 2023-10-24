21f45cead3969da13619565f8a8d1009bc1c7c3a
jaredgoldman <jaredgoldmandev@gmail.com>
2023-10-23 21:42:16 -0400

    fix: add random api token salt

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,15 @@
+const crypto = require("crypto");
+
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: crypto.randomBytes(16).toString("base64"),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("TRANSFER_TOKEN_SALT"),
     },
   },
 });
