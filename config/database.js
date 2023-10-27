e9752cb8a684c1abf21bd67d633d0cc2f6beacdc
jaredgoldman <jaredgoldmandev@gmail.com>
2023-10-26 22:39:10 -0400

    feat: update port again

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     client: "postgres",
     connection: {
       host: env("PGHOST", "strapidb"),
-      port: env.int("PGPORT", 5432),
+      port: env.int("PGPORT", 5433),
       database: env("PGDATABASE", "jazzintoronto"),
       user: env("PGUSER", "jazzintoronto"),
       password: env("PGPASSWORD", "password"),
