2a1fdc4aab93188d64458940bcaac82a45589bca
jaredgoldman <jaredgoldmandev@gmail.com>
2023-11-11 13:24:12 -0500

    fix: revert to psql

--- config/database.js
+++ config/database.js
@@ -1,40 +1,14 @@
-const path = require("path");
-
-module.exports = ({ env }) => {
-  const client = env("DATABASE_CLIENT", "postgres");
-
-  const connections = {
-    postgres: {
-      connection: {
-        client: "postgres",
-        connection: {
-          host: env("PGHOST", "strapidb"),
-          port: env.int("PGPORT", 5433),
-          database: env("PGDATABASE", "jazzintoronto"),
-          user: env("PGUSER", "jazzintoronto"),
-          password: env("PGPASSWORD", "password"),
-          ssl: env.bool(true),
-        },
-        pool: { min: 0 },
-      },
-    },
-    sqlite: {
-      connection: {
-        filename: path.join(
-          __dirname,
-          "..",
-          env("DATABASE_FILENAME", ".tmp/data.db")
-        ),
-      },
-      useNullAsDefault: true,
-    },
-  };
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+      host: env("PGHOST", "strapidb"),
+      port: env.int("PGPORT", 5433),
+      database: env("PGDATABASE", "jazzintoronto"),
+      user: env("PGUSER", "jazzintoronto"),
+      password: env("PGPASSWORD", "password"),
+      ssl: env.bool(true),
     },
-  };
-};
+    pool: { min: 0 },
+  },
+});
